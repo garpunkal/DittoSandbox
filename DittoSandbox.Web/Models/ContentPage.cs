ec134d8eb4aa7fc470c80f8785201e83bc208549
Gareth Wright <garpunkal@gmail.com>
2016-11-23 14:24:36 +0000

    Content Page / ListView

---
+++ DittoSandbox.Web/Models/ContentPage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DittoSandbox.Web.Models.Processors;
+using Our.Umbraco.Ditto;
+
+namespace DittoSandbox.Web.Models
+{
+    public class ContentPage : BaseModel
+    {
+        [Title]
+        public string Title { get; set; }
+        
+        public HtmlString BodyText { get; set; }
+
+        [PublishDate]
+        public DateTime PublishDate { get; set; }
+
+        public string Url { get; set; }
+
+        [DelimitedString]
+        public IEnumerable<string> Departments { get; set; }
+   
+    }
+}
